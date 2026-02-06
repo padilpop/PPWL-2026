0c5a010f330826ce602bc95dda0d9c0ef8f18083
Fahdil Raihandi <fahdilraihandi2146@gmail.com>
2026-02-06 10:34:32 +0700

    PPWL-W-1

---
+++ index.ts
@@ -0,0 +1,28 @@
+import { Elysia } from 'elysia';
+
+const app = new Elysia().listen(3000);
+
+app.get('/', () => 'Hello, Elysia!');
+
+console.log(
+  `🦊 Elysia running at http://localhost:${app.server?.port}`
+);
+
+app.get("/hello/:name", ({ params }) => {
+  return {
+    message: `Halo ${params.name}!`
+  };
+});
+
+app.post("/login", ({ body }) => {
+  const { email, password } = body as {
+    email: string;
+    password: string;
+  };
+
+
+  return {
+    success: true,
+    email
+  };
+});
